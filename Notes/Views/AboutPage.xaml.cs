f752fb9a082bc331066fe1f25186698ca15d5fc7
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2024-12-01 22:07:09 -0500

    Problema arreglado

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -1,11 +1,11 @@
-namespace Notes;
+namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
 {
 	public AboutPage()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+    }
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
         if (BindingContext is Models.About about)
