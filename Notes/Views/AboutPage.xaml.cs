d95780372c0dc8b60daf5b475cec009878849342
sol8888 <danisol8888@gmail.com>
2024-12-22 18:42:24 -0500

    Creacion del ViewModels

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -2,15 +2,8 @@ namespace Notes.Views;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-        InitializeComponent();
-    }
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    public AboutPage()
     {
-        if (BindingContext is Models.About about)
-        {
-            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
-        }
+        InitializeComponent();
     }
 }
